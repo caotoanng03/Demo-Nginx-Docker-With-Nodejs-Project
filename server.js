e6f24fe00b286f82a9be4ea2d02cb20dc5b063d1
Cao-Toan Nguyen <caotoan940@gmail.com>
2024-12-09 01:27:03 +0100

    setup docker-compose.yaml for running multiple servers

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const path = require('path')
 const app = express();
 const port = 8080;
 
+const appName = process.env.APP_NAME;
+
 app.use(express.static('public'))
 
 app.use('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-    console.log('Request served by Node app');
+    console.log(`Request served by Node ${appName}`);
 })
 
 app.listen(port, () => {
-    console.log(`app listening on port ${port}`);
+    console.log(`${appName} listening on port ${port}`);
 })
